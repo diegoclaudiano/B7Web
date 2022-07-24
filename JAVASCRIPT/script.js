462ac44c639aae0576c2a2addb61500facab6b62
Diêgo Claudiano <dgsilva.230493@gmail.com>
2022-07-24 17:06:48 -0300

    aula sobre output

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,6 @@
+document.getElementById("titulo").innerHTML = "Opa, tudo bem?";
+document.write("Algum texto");
+
+window.alert("Alguma coisa");
+
+console.log("A mensagem que eu escrever aqui aparecerá no console");
